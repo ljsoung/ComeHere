745c28cd54c7af9f8cf3dcfc5d0d8da18e6bfc90
ljsoung <ljsoung0923@sunmoon.ac.kr>
2026-03-13 15:53:11 +0900

    setting

---
+++ back-end/ym/src/main/java/com/jiseong/ym/YmApplication.java
@@ -0,0 +1,13 @@
+package com.jiseong.ym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YmApplication.class, args);
+    }
+
+}
---
+++ back-end/ym/src/test/java/com/jiseong/ym/YmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiseong.ym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
